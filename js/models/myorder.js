bbbd5f15c8f523a109c3ba5a9c0eec92e6788902
Sergey Sorokin <sergey.sorokin@revelsystems.com>
2017-09-07 13:33:10 +0300

    WOMA-899 Online Ordering: PPLC > 2 items with the same name > Both items are removed from the cart if change the price for one of them

--- js/models/myorder.js
+++ js/models/myorder.js
@@ -2213,7 +2213,8 @@ define(["backbone", 'total', 'checkout', 'products', 'rewards', 'stanfordcard'],
         price_changed: function(data) {
             // clear item(s) inside order
             var products = App.Data.myorder.filter( function(el) {
-                return (el.get_product().get('name') == data.name) ? el : null;
+                var product = el.get_product();
+                return (product.get('name') == data.name && product.get('id') == data.product_id) ? el : null;
             }).map( function(item_found) {
                 App.Data.myorder.remove(item_found);
                 var product = item_found.get_product();
